5e063956e0aaba90b3612dd458ad542940b5b957
Kayza170 <yannic.rogowitz@gmail.com>
2026-01-12 22:07:28 +0000

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },   // eslint NICHT mehr hier konfigurieren
+};
+module.exports = nextConfig;
